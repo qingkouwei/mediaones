cec475e1bfd5807b5351cc7e38d244ac5298ca16
shenjunwei001 <shenjunwei001@ke.com>
2020-10-27 16:59:00 +0800

    add android and qt samples

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
+void slots1() { cout << "slots1 call" << endl; }
+
+void slots2() { cout << "slots2 call" << endl; }
+
+struct Hello {
+  void operator()() const { std::cout << "Hello"; }
+};
+
 int main(int, char**) { std::cout << "Hello, world!\n"; }
